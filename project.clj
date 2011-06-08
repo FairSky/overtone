c4eab21af257e4cb71e1685709d3fc54576b6360
Jeff Rose <rosejn@gmail.com>
2011-06-08 19:31:19 +0200

    adding plasma dependency

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                  [overtone/osc-clj "0.3.0-SNAPSHOT"]
                  [overtone/byte-spec "0.2.0-SNAPSHOT"]
                  [overtone/midi-clj "0.2.0-SNAPSHOT"]
+                 [plasma "0.2.0-SNAPSHOT"]
                  [org.clojars.overtone/vijual "0.2.1"]]
   :dev-dependencies [[marginalia "0.2.0"]]
   :jvm-opts ["-Xms256m" "-Xmx1g" "-XX:+UseConcMarkSweepGC"])
